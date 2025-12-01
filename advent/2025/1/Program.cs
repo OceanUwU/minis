10dfd67ada34636ed75227b80f95946f22fde177
ocean <ocean@ocean.lol>
2025-12-01 13:51:05 +0000

    day 1 part 2

--- advent/2025/1/Program.cs
+++ advent/2025/1/Program.cs
@@ -9,13 +9,34 @@
             .Split("\n")
             .Where(static l => l != "")
             .Select(static l => int.Parse(l[1..]) * (l[0] == 'L' ? -1 : 1));
-        int dial = STARTING_DIAL_POSITION;
-        int timesStoppedAtZero = 0;
-        foreach (int r in rotations) {
-            dial = ((dial + r) % NUM_DIAL_POSITIONS + NUM_DIAL_POSITIONS) % NUM_DIAL_POSITIONS;
-            if (dial == 0)
-                timesStoppedAtZero++;
-        }
-        Console.WriteLine(timesStoppedAtZero);
+        Console.WriteLine(CountRestsAtZero(rotations));
+        Console.WriteLine(CountPassesofZero(rotations));
     }
+
+    static int CountRestsAtZero(IEnumerable<int> rotations) {
+        int dial = STARTING_DIAL_POSITION;
+        int rests = 0;
+        foreach (int r in rotations) {
+            dial = Mod(dial + r, NUM_DIAL_POSITIONS);
+            if (dial == 0)
+                rests++;
+        }
+        return rests;
+    }
+
+    static int CountPassesofZero(IEnumerable<int> rotations) {
+        int dial = STARTING_DIAL_POSITION;
+        int passes = 0;
+        foreach (int r in rotations) {
+            if (r == 0) continue;
+            if (dial == 0 && r < 0)
+                passes--;
+            dial += r;
+            passes += Math.Abs(dial / NUM_DIAL_POSITIONS) + (dial <= 0 ? 1 : 0);
+            dial = Mod(dial, NUM_DIAL_POSITIONS);
+        }
+        return passes;
+    }
+
+    static int Mod(int x, int y) => (x % y + y) % y;
 }
