91b63e05d6ebb482abfebf8b15a10f4270946d4e
ocean <ocean@ocean.lol>
2025-12-01 12:38:08 +0000

    day 1 part 1

---
+++ advent/2025/1/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program {
+    const int STARTING_DIAL_POSITION = 50;
+    const int NUM_DIAL_POSITIONS = 100;
+
+    static void Main(string[] args) {
+        StreamReader reader = new(args[0]);
+        string text = reader.ReadToEnd();
+        IEnumerable<int> rotations = text
+            .Split("\n")
+            .Where(static l => l != "")
+            .Select(static l => int.Parse(l[1..]) * (l[0] == 'L' ? -1 : 1));
+        int dial = STARTING_DIAL_POSITION;
+        int timesStoppedAtZero = 0;
+        foreach (int r in rotations) {
+            dial = ((dial + r) % NUM_DIAL_POSITIONS + NUM_DIAL_POSITIONS) % NUM_DIAL_POSITIONS;
+            if (dial == 0)
+                timesStoppedAtZero++;
+        }
+        Console.WriteLine(timesStoppedAtZero);
+    }
+}
