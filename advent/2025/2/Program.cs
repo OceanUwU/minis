ee58828ac8b063c835dea1b3fdf034bfd16946f8
ocean <ocean@ocean.lol>
2025-12-02 11:23:03 +0000

    day 2 part 1

---
+++ advent/2025/2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Range = (long Min, long Max);
+
+class Program {
+    static void Main(string[] args) {
+        StreamReader reader = new(args[0]);
+        string text = reader.ReadToEnd();
+        Range[] ranges = [..text
+            .Split(',')
+            .Select(static l => {
+                long[] nums = [..l.Split('-').Select(static n => long.Parse(n))];
+                return (nums[0], nums[1]);
+            })];
+        Console.WriteLine(GetInvalidIDsInRanges(ranges).Sum());
+    }
+
+    static List<long> GetInvalidIDsInRanges(Range[] ranges) {
+        List<long> invalids = [];
+        foreach (Range r in ranges)
+            invalids.AddRange(GetInvalidIDsInRange(r));
+        return invalids;
+    }
+
+    static List<long> GetInvalidIDsInRange(Range range) {
+        List<long> invalids = [];
+        for (long i = range.Min; i <= range.Max; i++) {
+            string str = i.ToString();
+            int len = str.Length;
+            if (len % 2 == 1) continue;
+            if (str[..(len/2)] == str[(len/2)..]) {
+                invalids.Add(i);
+                Console.WriteLine(i);
+            }
+        }
+        return invalids;
+    }
+}
